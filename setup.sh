77558f7bedb46ec51d3ed691b40610af917a37b7
FTP <67348935+ftpmorph@users.noreply.github.com>
2021-06-30 18:34:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 wget https://hydra.iohk.io/build/6797922/download/1/cardano-node-1.27.0-linux.tar.gz
 tar xzvf cardano-node-1.27.0-linux.tar.gz
-mkdir mainnetconfig && cd mainnetconfig
-wget https://hydra.iohk.io/build/6782523/download/1/mainnet-config.json
-wget https://hydra.iohk.io/build/6782523/download/1/mainnet-byron-genesis.json
-wget https://hydra.iohk.io/build/6782523/download/1/mainnet-shelley-genesis.json
-wget https://hydra.iohk.io/build/6782523/download/1/mainnet-alonzo-genesis.json
-wget https://hydra.iohk.io/build/6782523/download/1/mainnet-topology.json
-cd ..
+rm cardano-node-1.27.0-linux.tar.gz
 
 ./cardano-node run --topology ./mainnetconfig/mainnet-topology.json --database-path ./state-mainnet --port 3001
 --config ./mainnetconfig/mainnet-config.json --socket-path ~/cardano-mainnet.socket
