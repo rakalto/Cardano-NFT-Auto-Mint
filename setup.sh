7af41a84d9c49408e6e63b4d0138862c6bddf2c0
FTP <67348935+ftpmorph@users.noreply.github.com>
2021-06-30 19:03:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,42 +2,42 @@ wget https://hydra.iohk.io/build/6797922/download/1/cardano-node-1.27.0-linux.ta
 tar xzvf cardano-node-1.27.0-linux.tar.gz
 rm cardano-node-1.27.0-linux.tar.gz
 
-cardano-node run \
+./cardano-node run \
 --topology mainnet-topology.json \
 --database-path /db \
 --socket-path /db/node.socket \
 --host-addr $(ip -4 addr show ens3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') \
 --port 3001 \
---config mainnet-config.json
+--config configuration/cardano/mainnet-config.json
 
-mkdir payment
-cd payment
+mkdir nft-payment
+cd nft-payment
 
 cardano-cli address key-gen \
---verification-key-file payment.vkey \
---signing-key-file payment.skey
+--verification-key-file nftpayment.vkey \
+--signing-key-file nftpayment.skey
 
 cardano-cli address build \
---payment-verification-key-file payment.vkey \
---out-file payment.addr \
+--payment-verification-key-file nftpayment.vkey \
+--out-file nftpayment.addr \
 --mainnet
 
-echo $(cat ./payment.addr)
+echo $(cat ./nftpayment.addr)
 
 cd ..
 
-mkdir policy
+mkdir nft-policy
 
 cardano-cli address key-gen \
---verification-key-file policy/policy.vkey \
---signing-key-file policy/policy.skey
+--verification-key-file nft-policy/nftpolicy.vkey \
+--signing-key-file nft-policy/nftpolicy.skey
 
-touch policy/policy.script && echo "" > policy/policy.script
+touch nft-policy/nftpolicy.script && echo "" > nft-policy/nftpolicy.script
 
-echo "{" >> policy/policy.script 
-echo "  \"keyHash\": \"$(cardano-cli address key-hash --payment-verification-key-file policy/policy.vkey)\"," >> policy/policy.script 
-echo "  \"type\": \"sig\"" >> policy/policy.script 
-echo "}" >> policy/policy.script
+echo "{" >> nft-policy/nftpolicy.script 
+echo "  \"keyHash\": \"$(cardano-cli address key-hash --payment-verification-key-file nft-policy/nftpolicy.vkey)\"," >> nft-policy/nftpolicy.script 
+echo "  \"type\": \"sig\"" >> nft-policy/nftpolicy.script 
+echo "}" >> nft-policy/nftpolicy.script
 
-cardano-cli transaction policyid --script-file ./policy/policy.script >> policy/policyID
-echo $(cat policy/policyID)
+cardano-cli transaction policyid --script-file ./nft-policy/nftpolicy.script >> nft-policy/nftpolicyID
+echo $(cat nft-policy/nftpolicyID)
